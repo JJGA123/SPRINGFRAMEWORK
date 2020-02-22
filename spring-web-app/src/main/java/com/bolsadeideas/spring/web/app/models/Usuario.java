d63c09a5ecbb19f1433bff96c02031616cef328c
Jhon Galvis <jhon.galvis@norsoft.com.co>
2020-02-22 00:01:05 -0500

    Inicio de la aplicación.

---
+++ spring-web-app/src/main/java/com/bolsadeideas/spring/web/app/models/Usuario.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.spring.web.app.models;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+}
