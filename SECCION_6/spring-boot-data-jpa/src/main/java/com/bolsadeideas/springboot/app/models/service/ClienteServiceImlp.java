9260241900cc2c9fbf8ec4d5258c0f842a9aae78
Jhon Galvis <jhon.galvis@norsoft.com.co>
2020-03-17 01:34:23 -0500

    Fin sección 6.

--- SECCION_6/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImlp.java
+++ SECCION_6/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/service/ClienteServiceImlp.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.app.models.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,6 +43,12 @@ public class ClienteServiceImlp implements IClienteService{
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
 		clienteDao.deleteById(id);
+	}
+
+	@Override
+	public Page<Cliente> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return clienteDao.findAll(pageable);
 	}
 
 }
