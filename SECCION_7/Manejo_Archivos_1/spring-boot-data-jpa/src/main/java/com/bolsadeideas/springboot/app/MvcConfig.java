074005c10d676bc059a3bf4a4742bbbf05b6151c
Jhon Galvis <jhon.galvis@norsoft.com.co>
2020-03-19 22:48:12 -0500

    Fin sección 7.

---
+++ SECCION_7/Manejo_Archivos_1/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.bolsadeideas.springboot.app;
+
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toASCIIString();
+		registry.addResourceHandler("/uploads/**")
+			.addResourceLocations(resourcePath);
+		
+	}
+	
+}
