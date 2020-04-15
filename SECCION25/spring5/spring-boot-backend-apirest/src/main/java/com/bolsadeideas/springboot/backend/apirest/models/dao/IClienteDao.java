58d94a361c6b6be4036a642fed126eafd8789b52
Norsoft TFS SDX <jhongalvis2@gmail.com>
2020-04-15 00:22:35 -0500

    Back del API-REST

---
+++ SECCION25/spring5/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.bolsadeideas.springboot.backend.apirest.models.dao;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+
+}
